6eef2f3de9ed966077f4da6c34d913409062ca6a
Wenjie FU <fuwenjiexidian@gmail.com>
2021-11-17 23:04:54 +0100

    修改每个pom文件的<groupId>com.wenjie.crowd</groupId>

---
+++ atcrowdfunding03-admin-component/src/main/java/com/wenjie/crowd/service/api/AdminService.java
@@ -0,0 +1,11 @@
+package com.wenjie.crowd.service.api;
+import com.wenjie.crowd.entity.Admin;
+import com.wenjie.crowd.mapper.AdminMapper;
+
+/**
+ * @author Wenjie FU
+ * @create 2021-11-17 22:17
+ */
+public interface AdminService {
+    void saveAdmin(AdminMapper adminMapper);
+}
--- atcrowdfunding04-admin-entity/src/main/java/com/wenjie/crowd/entity/Admin.java
+++ atcrowdfunding04-admin-entity/src/main/java/com/wenjie/crowd/entity/Admin.java
@@ -14,7 +14,6 @@ public class Admin {
     private String createTime;
 
     public Admin() {
-        System.out.println("使用无参构造器初始化Admin");
     }
 
     public Admin(Integer id, String loginAcct, String userPswd, String userName, String email, String createTime) {
